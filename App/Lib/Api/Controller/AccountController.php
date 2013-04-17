97c9aec4fbe608a8edcfaf5cec41a05c702f2482
tianke <tianke890229@163.com>
2013-04-17 22:26:55 +0800

    更新

--- App/Lib/Api/Controller/AccountController.php
+++ App/Lib/Api/Controller/AccountController.php
@@ -3,10 +3,12 @@
 use Think\Controller as Controller;
 use Think\Request as Request;
 use Think\Lang as Lang;
+use Think\Session as Session;
 use \Exception;
 
 class AccountController extends Controller
 {
+	//登陆
 	public function login()
 	{
 		try {
@@ -14,7 +16,14 @@ class AccountController extends Controller
 			{
 				$data = M('User')->select();
 
-				$this->assign('success', true);
+				// 获取一个session_id
+				$access_token = session_id();
+
+				// 储存session
+				Session::set($access_token, $data['id']);
+
+				$data['access_token'] = $access_token;
+				$this->assign('success', 1);
 				$this->assign('data', $data);
 				$this->json();
 			}
@@ -23,10 +32,76 @@ class AccountController extends Controller
 			}
 		}
 		catch(Exception $error) {
-			$this->assign('success', false);
+			$this->assign('success', 0);
 			$this->assign('error', $error->getMessage());
 			$this->assign('error_msg', Lang::get($error->getMessage()));
 			$this->json();
+		}
+	}
+
+	//注册
+	public function register()
+	{
+		try {
+
+			if(Request::is('post'))
+			{
+				$User = M('User');
+
+				$User->username   = $_POST['username'];
+				$User->email      = $_POST['email'];
+				$User->password   = $_POST['password'];
+				$User->createline = time();
+
+				$User->add();
+
+				if($flag)
+				{
+					// $this->assign('success', 1);
+					// $this->assign('data', ？？？？);
+					// $this->json();
+				} 
+				else {
+					$this->assgin('success', 0);
+					$this->assgin('error', $error->getMessage());
+					$this->assgin('error_msg', Lang::get($error->getMessage()));
+				}
+			}
+			else {
+				throw new Exception("NO_POST_SUBMIT");
+			}
+
+		}
+		catch(Exception $error) {
+			$this->assgin('success', 0);
+			$this->assgin('error', $error->getMessage());
+			$this->assgin('error_msg', Lang::get($error->getMessage()));
+		}
+	}
+
+	//退出
+	public function logout() {
+		try {
+
+			if(Request::is('post'))
+			{
+				$condition['access_token'] = $_POST['access_token'];
+
+				//??????处理退出
+
+				$this->assgin('success', 1);
+				$this->json();
+
+			}
+			else {
+				throw new Exception("NO_POST_SUBMIT");
+			}
+
+		}
+		catch(Exception $error) {
+			$this->assgin('success', 0);
+			$this->assgin('error', $error->getMessage());
+			$this->assgin('error_msg', Lang::get($error->getMessage()));
 		}
 	}
 }
--- App/Lib/Api/Controller/FoodController.php
+++ App/Lib/Api/Controller/FoodController.php
@@ -7,8 +7,10 @@ use \Exception;
 class FoodController extends Controller
 {
 
+	//获取菜品列表，可根据店铺id查询
 	public function index()
 	{
+
 		try {
 
 			$condition = array();
@@ -18,7 +20,7 @@ class FoodController extends Controller
 				$condition['restaurant_id'] = $_GET['restaurant_id'];
 			}
 
-			$data = M('Goods')->where($condition)->select();
+			$data = M('Food')->where($condition)->select();
 
 			$this->assign('success', 1);
 			$this->assign('data', $data);
@@ -28,6 +30,88 @@ class FoodController extends Controller
 			$this->assign('success', 0);
 			$this->assign('error', $error->getMessage());
 			$this->assign('error_msg', Lang::get($error->getMessage()));
+			$this->json();
+		}
+	}
+
+	//获得店铺的菜品，按分类排列,可根据店铺id查询
+	public function shop()
+	{
+		try {
+			
+			// Food Category
+			$category = M('Category')->where(array('fid' => 1))->select();
+			$tempcat = array();
+			foreach ($category as $key => $value) {
+				$cid = $value['cid'];
+				$tempcat[$cid] = $value['name'];
+			}
+
+			// Food
+			$temp = array();
+			$condition = array();
+
+			if(isset($_GET['shop_id']))
+			{
+				$condition['shop_id'] = $_GET['shop_id'];
+			}
+
+			$datas = M('Food')->where($condition)->order('cid ASC')->select();
+
+			foreach ($datas as $key => $data) {
+				$cid = $data['cid'];
+				$temp[$cid]['cid'] = $cid ;
+				$temp[$cid]['name'] = $tempcat[$cid];
+				$temp[$cid]['food'][] = $data;
+			}
+
+			$temp = array_values($temp);
+			$this->assign('data', $temp);
+			$this->json();
+			// dump($temp);
+		} catch(Exception $error) {
+			$this->assgin('success', 0);
+			$this->assgin('error', $error->getMessage());
+			$this->assgin('error_msg', Lang::get($error->getMessage()));
+		}
+	}
+
+	//得用户吃过的菜品
+	public function favorite()
+	{
+		try{
+
+			$condition = array();
+
+			$condition['access_token'] = $_GET['access_token'];
+			//获取用户
+			$condition['user_id'] = Session::get('access_token');
+			//根据用户id,获取订单id
+			$temp = M(Orders)->where($condition)->getField('id');
+
+			if(!empty($temp))
+			{
+				//根据订单id，获取餐品id
+				$temp = M('OrdersFood')->where($temp)->getField('food_id');
+				//再根据餐品id,获取菜品信息
+				$data = M('Food')->where($temp)->select();
+
+				$this->assgin('success', 1);
+				$this->assgin('data', $data);
+				$this->json();
+			}
+			else {
+				$this->assgin('success', 0);
+				$this->assgin('error', 'NO_EXIST_ORDER');
+				$this->assgin('error_msg', '还没有订单');
+				$this->json();
+			}
+
+		}
+		catch(Exception $error) {
+			$this->assgin('success', 0);
+			$this->assgin('error', $error->getMessage());
+			$this->assgin('error_msg', Lang::get($error->getMessage()));
 			$this->json();
 		}
 	}
