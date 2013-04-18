9f1b9d1ec0a295b620d36f9df7aba4d2c7f8725b
MinoWu <minowu@foxmail.com>
2013-04-18 15:55:10 +0800

    添加session和access_token相关的行为和配置。

--- App/Lib/Api/Behaviors/CheckSessionId.php
+++ App/Lib/Api/Behaviors/CheckSessionId.php
@@ -6,11 +6,14 @@ class CheckSessionId extends Behavior
 {
 	public function run(&$params)
 	{
-		if(isset($_GET['access_token']))
+		// 合并get和post数据
+		$request = array_merge($_GET, $_POST);
+
+		// 检查get和post中是否存在access_token
+		// 存在，则将session_id设置为access_token的值
+		if(isset($request['access_token']))
 		{
-			session_id($_GET['access_token']);
-			unset($_GET['access_token']);
-			dump('ss');
+			session_id($request['access_token']);
 		}
 	}
 }
---
+++ App/Lib/Api/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'SESSION_OPTIONS' => array(
+		// session关闭cookie的自动使用
+		'use_cookies' => 0
+	)
+);
