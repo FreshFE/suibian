43715a63108856831da11b9009f6402908eceeda
Mino Wu <minowu@foxmail.com>
2013-05-03 22:10:37 +0800

    添加语言文件。

---
+++ App/Lib/Api/Lang/zh-cn.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+	'NO_LOGINED' => '没有用户登录',
+	'ERROR_REGISTER' => '注册认证失败',
+	'NO_EXTIS_USER' => '不存在该用户',
+	'NO_EXIST_FOOD' => '不存在该餐品',
+	'NO_EXIST_ORDER' => '没有订单',
+	'NO_FOODS' => '没有餐品',
+	'ERROR_ORDERS_CREATE' => '创建订单失败',
+	'ERROR_ORDERS' => '订单错误'
+);
