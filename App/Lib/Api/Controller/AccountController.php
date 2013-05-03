935fb243b91427acac28bfbd40530473ad8cd247
Mino Wu <minowu@foxmail.com>
2013-05-03 13:25:41 +0800

    删除测试信息。

--- App/Lib/Api/Controller/AccountController.php
+++ App/Lib/Api/Controller/AccountController.php
@@ -59,12 +59,8 @@ class AccountController extends Controller
 			$model = D('User');
 			$data = $model->create();
 
-			$this->json($_POST);
-
 			if($data)
 			{
-				$this->json($data);
-
 				$user_id = $model->add($data);
 
 				if($user_id)
