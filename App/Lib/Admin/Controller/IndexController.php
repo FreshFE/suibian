bad36bcabd6e6f1132d2616056b6b1fc6ffb9d8c
Mino Wu <minowu@foxmail.com>
2013-05-03 21:52:17 +0800

    修复跳转问题。

--- App/Lib/Admin/Controller/IndexController.php
+++ App/Lib/Admin/Controller/IndexController.php
@@ -2,11 +2,12 @@
 
 use Think\Controller as Controller;
 use Think\Redirect as Redirect;
+use Think\Url as Url;
 
 class IndexController extends Controller
 {
 	public function index()
 	{
-		Redirect::success('welcome');
+		Redirect::success('welcome', Url::make('food/index'));
 	}
 }
