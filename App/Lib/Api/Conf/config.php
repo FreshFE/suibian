ac1f8a73d80d6bb1189be5aa4e699b68743f7c2d
Mino Wu <minowu@foxmail.com>
2013-05-17 10:01:59 +0800

    将API中的检测Session和关闭cookie功能取消。

--- App/Lib/Api/Conf/config.php
+++ App/Lib/Api/Conf/config.php
@@ -1,9 +1,4 @@
 <?php
 
 return array(
-
-	'SESSION_OPTIONS' => array(
-		// session关闭cookie的自动使用
-		'use_cookies' => 0
-	)
 );
--- App/Lib/Api/Conf/tags.php
+++ App/Lib/Api/Conf/tags.php
@@ -1,9 +1,6 @@
 <?php
 
 return array(
-	'app_begin' => array(
-		'App\\Api\\Behaviors\\CheckSessionId',
-	),
 	'app_auth' => array(
 		'App\\Api\\Behaviors\\CheckAuth'
 	)
