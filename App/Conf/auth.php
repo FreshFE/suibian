568299927deb43d4b4727b7b0e33141cb7a628c5
Mino Wu <minowu@foxmail.com>
2013-05-20 16:02:28 +0800

    重新定义auth配置文件。

--- App/Conf/auth.php
+++ App/Conf/auth.php
@@ -2,67 +2,23 @@
 
 return array(
 
-	'AUTH_ON' => false,
+	// 用户角色开启
+	'AUTH_ON' => true,
 
-	'AUTH_KEY' => 'USER_AUTH_KEY',
-
+	// 用户角色
 	'AUTH_RULES' => array(
-
-		// Public
-		1 => array(
-		    'Home/Index/index' => true,
-
-		    'Home/Account/login' => true,
-		    'Home/Account/register' => true,
-		    'Home/Account/doregister' => true,
-		    'Home/Account/forget' => true,
-
-		    'Home/Recommend/index' => true,
-		    'Home/Recommend/clear' => true,
-		    'Home/Recommend/location' => true,
-
-		    'Admin/Account/login' => true
+		
+		'ROLE_ANONYMOUS' => array(
+			'Api:Account' => true
 		),
-
-		// Member
-		2 => array(
-		    'Home' => true,
-		    
-		    'Home/Account/login' => false,
-		    'Home/Account/register' => false,
-		    'Home/Account/doregister' => false,
-		    'Home/Account/forget' => false
+		'ROLE_MEMBER' => array(
+			'Api' => true,
+			'Api:Account:login' => false,
+			'Api:Account:register' => false,
 		),
-
-		// Superadmin
-		3 => array(
+		'ROLE_ADMIN' => array(
 			'Admin' => true,
-			'Admin/Account/login' => false
-		)
-	),
-
-	// 规则
-	'AUTH_ROLES' => array(
-
-		// Public
-		1 => array(
-			'name' => 'Public',
-			'adapter_id' => 1,
-			'basetype' => true
-		),
-
-		// Member
-    	2 => array(
-    		'name' => 'Member',
-    		'adapter_id' => 2,
-    		'basetype' => true
-		),
-
-    	// SuperAdmin
-		3 => array(
-			'name' => 'SpuerAdmin',
-			'adapter_id' => array(2,3),
-			'basetype' => true
+			'_extends' => array('ROLE_ADMIN')
 		)
 	)
 );
