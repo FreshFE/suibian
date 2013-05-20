e721a2aed00997e58d2ea2ee9e52da06d9f5c9e5
Mino Wu <minowu@foxmail.com>
2013-05-20 16:05:19 +0800

    完善check方法，添加getRuleByUser方法。

--- App/Lib/Auth/Drivers/Authorization.php
+++ App/Lib/Auth/Drivers/Authorization.php
@@ -14,7 +14,53 @@ class Authorization
 
 	public function check()
 	{
+		$rule = $this->getRuleByUser();
+
+		$routers = array(
+			GROUP_NAME . ':' . CONTROLLER_NAME . ':' . ACTION_NAME,
+			GROUP_NAME . ':' . CONTROLLER_NAME,
+			GROUP_NAME
+		);
+
+		foreach ($routers as $router) {
+			
+			$access = $rule[$router];
+
+			// 数组的话，执行指定类方法
+			if(is_array($access)) {
+
+				if($this->$access[1]()) return $access[0];
+			}
+
+			// 布尔值的话，返回
+			if(is_bool($access)) {
+
+				return $access;
+			}
+		}
+
+		return false;
+	}
+
+	public function getRuleByUser()
+	{
 		$roleRules = $this->roleRules;
 		$rule = $roleRules[$this->userRole];
+
+		// 没有角色继承模型
+		if(!isset($rule['_extends'])) {
+			return $rule;
+		}
+
+		// 合并角色继承
+		$extends = $rule['_extends'];
+		unset($rule['_extends']);
+		$temp = array();
+
+		foreach ($extends as $key => $value) {
+			$temp = array_merge($temp, $roleRules[$value]);
+		}
+
+		return array_merge($temp, $rule);
 	}
 }
