4182e7a40c09092c7692ee9e40b7263582ab07c1
Mino Wu <minowu@foxmail.com>
2013-05-20 16:02:53 +0800

    给checkAuth添加401和403方法。

--- App/Lib/Auth/Behaviors/CheckAuth.php
+++ App/Lib/Auth/Behaviors/CheckAuth.php
@@ -4,24 +4,14 @@ use Think\Behavior as Behavior;
 use Think\Config as Config;
 use Think\Session as Session;
 use Think\Cookie as Cookie;
+use Think\Response as Response;
 use Think\Exception as Exception;
-use Think\File as File;
 use App\Auth\Drivers\Authentication;
 use App\Auth\Drivers\Authorization;
 
 class CheckAuth extends Behavior
 {
-	public $roleRules = array(
-		'ROLE_ANONYMOUS' => array(
-			'Api/Account' => true
-		),
-		'ROLE_MEMBER' => array(
-			'Api' => true,
-			'Api/Account/login' => false,
-			'Api/Account/register' => false
-		),
-		'ROLE_ADMIN' => array(),
-	);
+	public $roleRules;
 
 	public $modelName = 'User';
 
@@ -33,9 +23,38 @@ class CheckAuth extends Behavior
 	 */
 	public function run(&$params)
 	{
+		// 设置用户角色的规则
+		if(is_null($roleRules)) {
+			$this->roleRules = Config::get('AUTH_RULES');
+		}
+
+		// 认证
 		$authentication = $this->checkAuthentication($this->modelName);
 
+		// 授权
 		$authorization = $this->checkAuthorization($authentication->getUserRole(), $this->roleRules);
+
+		// 授权未通过
+		if(!$authorization) {
+			if($authentication->getUserRole() == 'ROLE_ANONYMOUS') {
+				$this->redirectTo401();
+			}
+			else {
+				$this->redirectTo403();
+			}
+		}
+	}
+
+	public function redirectTo401()
+	{
+		Response::send_http_status(401);
+		exit();
+	}
+
+	public function redirectTo403()
+	{
+		Response::send_http_status(403);
+		exit();
 	}
 
 	protected function checkAuthentication($modelName)
