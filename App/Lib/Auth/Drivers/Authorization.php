f0c5ec9c2b911b2534c74686a8c5257668224504
Mino Wu <minowu@foxmail.com>
2013-05-20 15:03:48 +0800

    设置Authorization的$userRole，$roleUsers属性，构造函数和check方法。

--- App/Lib/Auth/Drivers/Authorization.php
+++ App/Lib/Auth/Drivers/Authorization.php
@@ -2,5 +2,19 @@
 
 class Authorization
 {
+	protected $userRole;
 
+	protected $roleRules;
+
+	public function __construct($userRole, $roleRules)
+	{
+		$this->userRole = $userRole;
+		$this->roleRules = $roleRules;
+	}
+
+	public function check()
+	{
+		$roleRules = $this->roleRules;
+		$rule = $roleRules[$this->userRole];
+	}
 }
