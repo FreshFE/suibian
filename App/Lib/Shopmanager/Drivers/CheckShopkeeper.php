1f97eb4e8eabcaa2e9648477a8d33130e1364c71
Mino Wu <minowu@foxmail.com>
2013-05-21 15:50:37 +0800

    定义CheckShopkeeper内的检查。

--- App/Lib/Shopmanager/Drivers/CheckShopkeeper.php
+++ App/Lib/Shopmanager/Drivers/CheckShopkeeper.php
@@ -1,9 +1,20 @@
 <?php namespace App\Shopmanager\Drivers;
 
+use App\Auth\Drivers\UserSession;
+
 class CheckShopkeeper
 {
 	public function run()
 	{
-		return true;
+		$user = UserSession::getAll();
+
+		$data = D('ShopManager')->where(array('user_id' => $user['id']))->find();
+
+		if($data) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
