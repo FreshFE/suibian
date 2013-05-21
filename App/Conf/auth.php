98ca94613cd3f08843e16943763b270aaa12c208
Mino Wu <minowu@foxmail.com>
2013-05-21 15:22:12 +0800

    修改权限认证表。

--- App/Conf/auth.php
+++ App/Conf/auth.php
@@ -9,19 +9,43 @@ return array(
 	'AUTH_RULES' => array(
 		
 		'ROLE_ANONYMOUS' => array(
+
+			// Home
 			'Home' => true,
+
+			// Shopmanager
+			'Shopmanager:Account:login' => true,
+			'Shopmanager:Account:register' => true,
+			'Shopmanager:Account:logout' => false,
+
+			// Api
 			'Api:Account' => true,
-			'Api:Account:logout' => false
+			'Api:Account:logout' => false,
+			'Api:Shop' => true
 		),
 		'ROLE_MEMBER' => array(
+
+			// Home
 			'Home' => true,
+
+			// Api
 			'Api' => true,
 			'Api:Account:login' => false,
 			'Api:Account:register' => false,
 		),
+		'ROLE_SHOPKEEPER' => array(
+
+			// Shopmanager
+			'Shopmanager' => array('App\\Shopmanager\\Drivers\\CheckShopkeeper', true),
+			// 'Shopmanager' => true,
+			'Shopmanager:Account:login' => false,
+			'Shopmanager:Account:register' => false,
+
+			'_extends' => array('ROLE_MEMBER')
+		),
 		'ROLE_ADMIN' => array(
 			'Admin' => true,
-			'_extends' => array('ROLE_ADMIN')
+			'_extends' => array('ROLE_MEMBER')
 		)
 	)
 );
