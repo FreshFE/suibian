91f47e18e4b00448e4491533bf582abd79d6d4f8
Mino Wu <minowu@foxmail.com>
2013-05-22 09:59:19 +0800

    改变用户信息的获得方式。

--- App/Lib/Shopmanager/Drivers/CheckShopkeeper.php
+++ App/Lib/Shopmanager/Drivers/CheckShopkeeper.php
@@ -1,16 +1,24 @@
 <?php namespace App\Shopmanager\Drivers;
 
-use App\Auth\Drivers\UserSession;
+use Think\Request;
+use Think\Response;
 
 class CheckShopkeeper
 {
 	public function run()
 	{
-		$user = UserSession::getAll();
+		$user = Request::getStorage('user');
 
 		$data = D('ShopManager')->where(array('user_id' => $user['id']))->find();
 
 		if($data) {
+
+			// 查询商店
+			$shop = D('Shop')->where(array('id' => $data['shop_id']))->find();
+
+			// 存储全局变量
+			Request::setStorage('shop', $shop);
+
 			return true;
 		}
 		else {
