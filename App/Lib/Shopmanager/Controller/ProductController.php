42a9993c32554c83fda12635934dafabc8060622
Mino Wu <minowu@foxmail.com>
2013-05-23 09:15:41 +0800

    调整注释。

--- App/Lib/Shopmanager/Controller/ProductController.php
+++ App/Lib/Shopmanager/Controller/ProductController.php
@@ -7,18 +7,51 @@ use Think\Url;
 use Think\Response;
 use Think\Library\Upload\Upload;
 
+/**
+ * 产品控制器
+ */
 class ProductController extends CommonController
 {
+	/**
+	 * 模型存放
+	 *
+	 * @var object
+	 */
 	protected $model;
 
+	/**
+	 * 模型名
+	 *
+	 * @var string
+	 */
 	protected $model_name = 'Product';
 
+	/**
+	 * 上传封面图片的尺寸
+	 *
+	 * @string
+	 */
 	protected $cover_thumb_name = '400x400,480x800,thumb';
 
+	/**
+	 * 主键名称
+	 *
+	 * @var string
+	 */
 	protected $pk_name = 'id';
 
+	/**
+	 * 主键存放
+	 *
+	 * @var int
+	 */
 	protected $pk_id;
 
+	/**
+	 * 构造函数
+	 *
+	 * @return void
+	 */
 	public function __construct()
 	{
 		parent::__construct();
@@ -37,15 +70,31 @@ class ProductController extends CommonController
 		}
 	}
 
+	/**
+	 * 首页方法
+	 *
+	 * @return void
+	 */
 	public function index()
 	{
-		$model = $this->getModel('Product');
+		// 筛选条件
 		$condition['shop_id'] = $this->getShopId();
-		$datas = $model->where($condition)->select();
+
+		// 分类筛选
+		if(Request::query('product_category_id')) {
+			$condition['product_category_id'] = Request::query('product_category_id');
+		}
+
+		$datas = $this->model->where($condition)->select();
 		$this->assign('datas', $datas);
 		$this->display();
 	}
 
+	/**
+	 * 添加一个新的记录
+	 *
+	 * @return void
+	 */
 	public function add()
 	{
 		$product_category_id = $this->getModel('ProductCategory')->where(array('shop_id' => $this->getShopId()))->getField('id');
@@ -97,6 +146,11 @@ class ProductController extends CommonController
 		}
 	}
 
+	/**
+	 * 编辑
+	 *
+	 * @return void
+	 */
 	public function post_edit()
 	{
 		$model = $this->getModel('Product');
@@ -105,11 +159,11 @@ class ProductController extends CommonController
 		Redirect::success('编辑成功', Url::make('index'));
 	}
 
-	// public function sidebar()
-	// {
-	// 	return ;
-	// }
-
+	/**
+	 * 获得分类数据
+	 *
+	 * @return void
+	 */
 	public function category()
 	{
 		return $this->getModel('ProductCategory')->where(array('shop_id' => $this->getShopId()))->select();
