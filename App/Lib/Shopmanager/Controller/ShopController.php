483f18f8eeab11dfc022e72111dccb81fb59fe26
Mino Wu <minowu@foxmail.com>
2013-05-23 14:38:29 +0800

    简历Shop的cover接口。

--- App/Lib/Shopmanager/Controller/ShopController.php
+++ App/Lib/Shopmanager/Controller/ShopController.php
@@ -1,20 +1,37 @@
 <?php namespace App\Shopmanager\Controller;
 
-use Think\Controller;
+use Think\Library\Upload\Upload;
+use Think\Response;
 
-class ShopController extends Controller
+class ShopController extends CommonController
 {
+	protected $cover_thumb_name = '400x400,480x800,thumb';
+	
 	public function index()
 	{
 		$this->display();
 	}
 
-	public function get_face()
+	public function face()
 	{
+		$data = $this->getModel('Shop')->find($this->getShopId());
+		$this->assign('data', $data);
 		$this->display();
 	}
 
-	public function post_face()
+	public function cover()
 	{
+		// 上传图片
+		$info = Upload::image($_FILES['uploadify_file'], $this->cover_thumb_name);
+
+		// 建立数据表
+		$id = $this->getModel('Shop')->where(array('id' => $this->getShopId()))->save(array('coverpath' => $info['name']));
+
+		if(!$id) {
+			Response::json(array("success" => 0));
+		}
+
+		// 输出JSON
+		Response::json($info);
 	}
 }
