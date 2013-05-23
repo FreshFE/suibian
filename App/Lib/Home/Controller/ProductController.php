54fc773e716051d1cdadc191c68cb663a05ed53b
Mino Wu <minowu@foxmail.com>
2013-05-23 20:57:57 +0800

    添加Product的测试案例。

---
+++ App/Lib/Home/Controller/ProductController.php
@@ -0,0 +1,11 @@
+<?php namespace App\Home\Controller;
+
+use Think\Controller as Controller;
+
+class ProductController extends Controller
+{
+	public function index()
+	{
+		$this->display();
+	}
+}
