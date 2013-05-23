3325fb2727eb0a75b7e8a87daaa2cef890836f67
Mino Wu <minowu@foxmail.com>
2013-05-23 13:32:02 +0800

    初始化Shop的face接口。

--- App/Lib/Shopmanager/Controller/ShopController.php
+++ App/Lib/Shopmanager/Controller/ShopController.php
@@ -6,6 +6,15 @@ class ShopController extends Controller
 {
 	public function index()
 	{
-		
+		$this->display();
+	}
+
+	public function get_face()
+	{
+		$this->display();
+	}
+
+	public function post_face()
+	{
 	}
 }
