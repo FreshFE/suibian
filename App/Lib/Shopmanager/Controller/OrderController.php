deef70cb63b2c8c33aca226466216cafa832c14b
MinoWu <minowu@foxmail.com>
2013-05-27 11:51:29 +0800

    完成订单功能。

--- App/Lib/Shopmanager/Controller/OrderController.php
+++ App/Lib/Shopmanager/Controller/OrderController.php
@@ -31,4 +31,54 @@ class OrderController extends CommonController
 		$this->assign('datas', $datas);
 		$this->display();
 	}
+
+	public function detail()
+	{
+		$model = $this->getModel('Orders');
+
+		// 是否存在有效ID
+		if(!Request::query('id')) {
+			exit('NO_GET_ORDER_ID');
+		}
+
+		// 查询，获取并赋值
+		$condition['id'] = Request::query('id');
+		$data = $model->where($condition)->find();
+		
+
+		// 获得菜品相关
+		$data['products'] = $this->getModel('OrdersProduct')
+							->join('product ON orders_product.product_id = product.id')
+							->where('orders_product.orders_id = ' . $condition['id'])
+							->select();
+
+		$this->assign('data', $data);
+		$this->display();
+	}
+
+	public function confirm()
+	{
+		$model = $this->getModel('Orders');
+
+		// 是否存在有效ID
+		if(!Request::post('id') || !Request::post('status')) {
+			exit('NO_POST_ORDER');
+		}
+
+		$id = $model
+			->where(array('id' => Request::post('id')))
+			->save(array('status' => Request::post('status')));
+
+		if($id) {
+			Response::json(array(
+				'success' => 1,
+				'data' => array('id' => Request::post('id'))
+			));
+		}
+		else {
+			Response::json(array(
+				'success' => 0
+			));
+		}
+	}
 }
