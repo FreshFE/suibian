184b28c5f1636384984d316c6327c945332f102a
MinoWu <minowu@foxmail.com>
2013-05-30 16:02:18 +0800

    编辑登录。

--- App/Lib/Home/Controller/IndexController.php
+++ App/Lib/Home/Controller/IndexController.php
@@ -1,11 +1,32 @@
 <?php namespace App\Home\Controller;
 
 use Think\Controller;
+use Think\Request;
+use Think\Auths\UserSession;
 
 class IndexController extends Controller
 {
 	public function index()
 	{
+		if(Request::getStorage('user')) {
+
+			$user = UserSession::get();
+
+			// 查询该用户是否有管理商店
+			$data = $this->getModel('ShopManager')->where(array('user_id' => $user['id']))->find();
+
+			if($data) {
+				$this->assign('role_shop_manager', true);
+			}
+
+			// 查询该用户是否具有超级管理员权限
+			if($user['role'] === 'ROLE_ADMIN') {
+				$this->assign('role_admin', true);
+			}
+
+			$this->assign('role', true);
+		}
+
 		$this->display();
 	}
 }
