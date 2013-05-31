f5ea7da882d53e4cb98182dd781a95128bd6d064
MinoWu <minowu@foxmail.com>
2013-05-31 15:39:10 +0800

    更新首页。

--- App/Lib/Home/Controller/IndexController.php
+++ App/Lib/Home/Controller/IndexController.php
@@ -8,27 +8,6 @@ class IndexController extends Controller
 {
 	public function index()
 	{
-		if(Request::getStorage('user')) {
-
-			$user = UserSession::get();
-
-			// 查询该用户是否有管理商店
-			$data = $this->getModel('ShopManager')->where(array('user_id' => $user['id']))->find();
-
-			if($data) {
-				$this->assign('role_shop_manager', true);
-			}
-
-			// 查询该用户是否具有超级管理员权限
-			if($user['role'] === 'ROLE_ADMIN') {
-				$this->assign('role_admin', true);
-			}
-
-			$this->assign('role', true);
-
-			$this->assign('user', $user);
-		}
-
 		$this->display();
 	}
 }
