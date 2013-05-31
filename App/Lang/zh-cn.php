24cfdde15c4b99961bea8f6c8ee502cc173b18af
MinoWu <minowu@foxmail.com>
2013-05-31 18:30:33 +0800

    改变语言包路径，并添加项目。

--- App/Lang/zh-cn.php
+++ App/Lang/zh-cn.php
@@ -9,5 +9,8 @@ return array(
 	'NO_EXIST_ORDER' => '没有订单',
 	'NO_FOODS' => '没有餐品',
 	'ERROR_ORDERS_CREATE' => '创建订单失败',
-	'ERROR_ORDERS' => '订单错误'
+	'ERROR_ORDERS' => '订单错误',
+	'NO_EXIST_USER' => '不存在该用户',
+	'NO_POST_PASSWORD' => '没有输入密码',
+	'ERROR_PASSWORD' => '您输入的密码错误'
 );
