4d2bd0ea8e441cec04ae8da120c4e123e0c4e13c
MinoWu <minowu@foxmail.com>
2013-05-31 19:06:01 +0800

    加入新的翻译内容。

--- App/Lang/zh-cn.php
+++ App/Lang/zh-cn.php
@@ -12,5 +12,16 @@ return array(
 	'ERROR_ORDERS' => '订单错误',
 	'NO_EXIST_USER' => '不存在该用户',
 	'NO_POST_PASSWORD' => '没有输入密码',
-	'ERROR_PASSWORD' => '您输入的密码错误'
+	'ERROR_PASSWORD' => '您输入的密码错误',
+
+	'NO_EMAIL' => '没有输入电子邮件',
+	'NO_USERNAME' => '没有输入用户名',
+	'EXTIS_EMAIL' => '已经存在该电子邮件',
+	'EXTIS_USERNAME' => '已经存在该用户名',
+
+	'NO_SCHOOL' => '没有输入学校',
+	'NO_ADDRESS' => '没有输入地址',
+	'NO_RECEIVER' => '没有输入收件人',
+	'NO_PHONE' => '没有输入电话号码',
+
 );
